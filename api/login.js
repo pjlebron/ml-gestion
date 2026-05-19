c9e3662aa26a13cd4916a6d127a68bce1d43c5c4
pjlebron <pjlebron@gmail.com>
2026-05-19 13:37:36 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,5 +1,10 @@
+import { normalizeAccount } from './_token.js';
+
 export default function handler(req, res) {
   const { ML_APP_ID, ML_REDIRECT_URI } = process.env;
-  const url = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${ML_APP_ID}&redirect_uri=${encodeURIComponent(ML_REDIRECT_URI)}`;
+  const account = normalizeAccount(req.query.account || 'lebron');
+
+  const url = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${ML_APP_ID}&redirect_uri=${encodeURIComponent(ML_REDIRECT_URI)}&state=${encodeURIComponent(account)}`;
+
   res.redirect(url);
 }
